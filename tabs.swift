eb1d254bd2d30e4bd6b4a668c07eb5285b79a62e
James Addyman <james@rantmedia.com>
2020-01-13 10:52:54 +0000

    tabs

---
+++ tabs.swift
@@ -0,0 +1,8 @@
+class Tabs {
+let remoteDataSource = RemoteDataSourceImplementation(baseURL: baseURL,
+															  azureBaseURL: azureBaseURL,
+															  clientID: clientID,
+															  clientSecret: clientSecret,
+															  networkActivityIndicator: UIApplication.shared)
+		localNotificationsManager = LocalNotificationsManagerImplementation(userNotificationCenter: .current())
+}
